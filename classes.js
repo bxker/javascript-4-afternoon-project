5281855d8c220d04f1e7d67326e1940b062b0f49
bxker <jake.luke.baker@gmail.com>
2019-08-16 15:45:23 -0500

    working on black diamond

--- classes.js
+++ classes.js
@@ -170,5 +170,28 @@ class ProgressiveManager extends Manager {
 */
 
 //Code Here
+class Machine {
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    this.wear_and_tear_count = parseInt(this.widgets_made_count / 50);
+  }
+
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+
+  reboot(){
+  // function(){
+  //     this.wear_and_tear_count -= 10;
+  //     this.needs_reboot = false;
+  //   }
+  }
+}
 
 
